4171c706dc2ce017a2b6b3e0650c3585864a1a5a
furrybluelan <supollad@proton.me>
2024-12-28 22:21:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let isGenerating = false;
 
 const API_CONFIG = {
     'workers-api': {
-        url: 'https://1220.tts-api.zwei.de.eu.org/tts',
+        url: 'https://ttsapi.frbl.us.kg/tts'，
         authToken: 'your-key'
     },
     'deno-api': {
